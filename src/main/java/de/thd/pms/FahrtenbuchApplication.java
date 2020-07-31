00585ff9e8e3c121062df2ea901d7289f03acbc5
J. Schneeberger <josef@dr-schneeberger.de>
2020-07-31 19:18:25 +0200

    Some housekeeping at the end of semester 2020

--- src/main/java/de/thd/pms/FahrtenbuchApplication.java
+++ src/main/java/de/thd/pms/FahrtenbuchApplication.java
@@ -1,12 +1,17 @@
 package de.thd.pms;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FahrtenbuchApplication {
+	@SuppressWarnings("unused")
+	private static Logger log = LogManager.getLogger(FahrtenbuchApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(FahrtenbuchApplication.class, args);
 	}
+
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,1 @@
+# may be used to insert data into the database
